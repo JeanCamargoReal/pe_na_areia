50183013c94fce5b4cbec8b06702d9e21a266d25
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-03 09:51:53 -0300

    garantindo um contador de chamadas para nossa camada de NetworkClient

--- RestaurantDomain/RestaurantDomainTests/RestaurantDomainTests.swift
+++ RestaurantDomain/RestaurantDomainTests/RestaurantDomainTests.swift
@@ -17,14 +17,25 @@ final class RestaurantDomainTests: XCTestCase {
 
 		sut.load()
 
-		XCTAssertEqual(client.urlRequest, anyURL)
+		XCTAssertEqual(client.urlRequests, [anyURL])
+	}
+
+	func test_load_twice() throws {
+		let anyURL =  try XCTUnwrap(URL(string: "https://www.globo.com"))
+		let client = NetworkClientSpy()
+		let sut = RemoteRestaurantLoader(url: anyURL, networkClient: client)
+
+		sut.load()
+		sut.load()
+
+		XCTAssertEqual(client.urlRequests, [anyURL, anyURL])
 	}
 }
 
 final class NetworkClientSpy: NetworkClient {
-	private(set) var urlRequest: URL?
+	private(set) var urlRequests: [URL] = []
 
 	func request(from url: URL) {
-		urlRequest = url
+		urlRequests.append(url)
 	}
 }
