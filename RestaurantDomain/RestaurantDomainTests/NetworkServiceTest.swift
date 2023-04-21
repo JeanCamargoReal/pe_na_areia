f6ba2c14cc17e1dbdae4377c077e30a7f9cf4409
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-21 15:16:42 -0300

    criando makeSUT para reduzir duplicação de código

--- RestaurantDomain/RestaurantDomainTests/NetworkServiceTest.swift
+++ RestaurantDomain/RestaurantDomainTests/NetworkServiceTest.swift
@@ -9,45 +9,83 @@ import XCTest
 @testable import RestaurantDomain
 
 final class NetworkServiceTests: XCTestCase {
-
-
+	
 	func test_loadRequest_resume_dataTask_with_url() {
+		let (sut, session) = makeSUT()
 		let url = URL(string: "https://globo.com")!
-		let session = URLSessionSpy()
 		let task = URLSessionDataTaskSpy()
-		let sut = NetworkService(session: session)
-
+		
 		session.stub(url: url, task: task)
-
+		
 		sut.request(from: url) { _ in }
-
+		
 		XCTAssertEqual(task.resumeCount, 1)
 	}
-
+	
 	func test_loadRequest_and_completion_with_error() {
+		let (sut, session) = makeSUT()
 		let url = URL(string: "https://globo.com")!
-		let session = URLSessionSpy()
 		let task = URLSessionDataTaskSpy()
-		let sut = NetworkService(session: session)
 		let anyError = NSError(domain: "any error", code: -1)
-
+		
 		session.stub(url: url, task: task, error: anyError)
-
+		
 		let exp = expectation(description: "aguradando retorno da closure")
-
+		
 		sut.request(from: url) { result in
 			switch result {
 				case let .failure(returnedError):
 					XCTAssertEqual(returnedError as NSError, anyError)
-
+					
 				default:
 					XCTFail("Esperado falha, porem retornou \(result)")
 			}
 			exp.fulfill()
 		}
-
+		
 		wait(for: [exp], timeout: 1.0)
-
+	}
+	
+	func test_loadRequest_and_completion_with_success() {
+		let (sut, session) = makeSUT()
+		let url = URL(string: "https://globo.com")!
+		let task = URLSessionDataTaskSpy()
+		let data = Data()
+		let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+		
+		session.stub(url: url, task: task, data: data, response: response)
+		
+		let exp = expectation(description: "aguardando retorno da closure")
+		
+		sut.request(from: url) { result in
+			switch result {
+				case let .success((returnedData, returnedResponse)):
+					XCTAssertEqual(returnedData, data)
+					XCTAssertEqual(returnedResponse, response)
+					
+				default:
+					XCTFail("Esperado sucesso, porem retornou \(result)")
+			}
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
+	}
+	
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: NetworkClient, session: URLSessionSpy) {
+		let session = URLSessionSpy()
+		let sut = NetworkService(session: session)
+		
+		trackForMemoryLeaks(session)
+		trackForMemoryLeaks(sut)
+		
+		return (sut, session)
+	}
+	
+	private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+		addTeardownBlock { [weak instance] in
+			XCTAssertNil(instance, "A instância deveria ter sido desalocada, possível vazamento de memória.", file: file, line: line)
+		}
 	}
 }
 
@@ -59,10 +97,13 @@ final class URLSessionSpy: URLSession {
 	struct Stub {
 		let task: URLSessionDataTask
 		let error: Error?
+		let data: Data?
+		let response: HTTPURLResponse?
 	}
 
-	func stub(url: URL, task: URLSessionDataTask, error: Error? = nil) {
-		stubs[url] = Stub(task: task, error: error)
+
+	func stub(url: URL, task: URLSessionDataTask, error: Error? = nil, data: Data? = nil, response: HTTPURLResponse? = nil) {
+		stubs[url] = Stub(task: task, error: error, data: data, response: response)
 	}
 
 	override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
@@ -71,7 +112,7 @@ final class URLSessionSpy: URLSession {
 			return FakeURLSessionDataTask()
 		}
 
-		completionHandler(nil, nil, stub.error)
+		completionHandler(stub.data, stub.response, stub.error)
 
 		return stub.task
 	}
