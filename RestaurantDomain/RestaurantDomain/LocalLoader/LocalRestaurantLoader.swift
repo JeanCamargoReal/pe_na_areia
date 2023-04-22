9a9b2d93e6a5b82edabf6b7ec93f0e97db75d26d
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-22 11:12:24 -0300

    criando pastas para ajudar na divisão de regras de negócio e criando classes LocalRestaurantLoader e LocalRestaurantLoaderTest

---
+++ RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
@@ -0,0 +1,8 @@
+//
+//  LocalRestaurantLoader.swift
+//  RestaurantDomain
+//
+//  Created by Jean Camargo on 22/04/23.
+//
+
+import Foundation
---
+++ RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  LocalRestaurantLoaderTests.swift
+//  RestaurantDomainTests
+//
+//  Created by Jean Camargo on 22/04/23.
+//
+
+import Foundation
