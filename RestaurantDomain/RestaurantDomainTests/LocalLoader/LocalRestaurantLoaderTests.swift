8f95c237467d11b012882f6914b67bc356c66a94
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-28 16:24:02 -0300

    criando cenário de validação onde salvamos novos dados após o sucesso de deletar dados antigos

--- RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
+++ RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
@@ -20,17 +20,40 @@ final class LocalRestaurantLoaderTests: XCTestCase {
 
 		XCTAssertEqual(cache.deleteCount, 1)
 	}
+
+	func test_saveCommand_insert_new_data_on_cache() {
+		let currentDate = Date()
+		let cache = CacheClientSpy()
+		let sut = LocalRestaurantLoader(cache: cache, currentDate: { currentDate })
+		let items = [RestaurantItem(id: UUID(), name: "name", location: "location", distance: 5.5, ratings: 0, parasols: 0)]
+
+		sut.save(items) { _ in }
+
+		cache.completionHandlerForDelete(nil)
+
+		XCTAssertEqual(cache.deleteCount, 1)
+		XCTAssertEqual(cache.saveCount, 1)
+	}
 }
 
 final class CacheClientSpy: CacheClient {
 
-	func save(_ items: [RestaurantDomain.RestaurantItem], timestamp: Date, completion: @escaping (Error?) -> Void) { }
+	private(set) var saveCount = 0
+
+	func save(_ items: [RestaurantDomain.RestaurantItem], timestamp: Date, completion: @escaping (Error?) -> Void) {
+		saveCount += 1
+	}
 
 	private(set) var deleteCount = 0
+	private var completionHandler: ((Error?) -> Void)?
 
 	func delete(completion: @escaping (Error?) -> Void) {
 		deleteCount += 1
+		completionHandler = completion
 	}
 
+	func completionHandlerForDelete(_ error: Error?) {
+		completionHandler?(error)
+	}
 
 }
