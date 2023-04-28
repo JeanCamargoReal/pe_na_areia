811078d77ea9ad7a3a7fd3a324e837c5a13c39e6
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-04-28 16:08:30 -0300

    criando cenário de teste onde deletamos o cache antigo

--- RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
+++ RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
@@ -5,4 +5,32 @@
 //  Created by Jean Camargo on 22/04/23.
 //
 
-import Foundation
+@testable import RestaurantDomain
+import XCTest
+
+final class LocalRestaurantLoaderTests: XCTestCase {
+
+	func test_save_deletes_old_cache() {
+		let currentDate = Date()
+		let cache = CacheClientSpy()
+		let sut = LocalRestaurantLoader(cache: cache, currentDate: { currentDate })
+		let items = [RestaurantItem(id: UUID(), name: "name", location: "location", distance: 5.5, ratings: 0, parasols: 0)]
+
+		sut.save(items) { _ in }
+
+		XCTAssertEqual(cache.deleteCount, 1)
+	}
+}
+
+final class CacheClientSpy: CacheClient {
+
+	func save(_ items: [RestaurantDomain.RestaurantItem], timestamp: Date, completion: @escaping (Error?) -> Void) { }
+
+	private(set) var deleteCount = 0
+
+	func delete(completion: @escaping (Error?) -> Void) {
+		deleteCount += 1
+	}
+
+
+}
