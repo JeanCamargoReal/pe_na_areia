34a433043c1beb9c4cb4fbe4cea034ce88343fd9
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-01 12:20:58 -0300

    alterando unowned para self e criando teste que garante que nossa ação de salvar não é executada casa nosso LocalRestaurantLoader saia de memória

--- RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
+++ RestaurantDomain/RestaurantDomainTests/LocalLoader/LocalRestaurantLoaderTests.swift
@@ -75,6 +75,18 @@ final class LocalRestaurantLoaderTests: XCTestCase {
 		}
 	}
 
+	func test_save_non_insert_after_sut_deallocated() {
+		let currentDate = Date()
+		let cache = CacheClientSpy()
+		var sut: LocalRestaurantLoader? = LocalRestaurantLoader(cache: cache, currentDate: { currentDate })
+		let items = [makeItem()]
+
+		sut?.save(items) { _ in }
+		sut = nil
+
+		cache.completionHandlerForDelete(nil)
+	}
+
 	private func makeSUT(currentDate: Date = Date(), file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalRestaurantLoader, cache: CacheClientSpy) {
 
 		let cache = CacheClientSpy()
