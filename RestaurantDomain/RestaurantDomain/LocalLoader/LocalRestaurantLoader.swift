3baf1908dc39ebea2f4ccdd92b1e995d5678aa49
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-02 10:39:16 -0300

    expondo classes e protocolos para public se necessário

--- RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
+++ RestaurantDomain/RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
@@ -23,21 +23,21 @@
 
 import Foundation
 
-protocol CacheClient {
+public protocol CacheClient {
 	func save(_ items: [RestaurantItem], timestamp: Date, completion: @escaping (Error?) -> Void)
 	func delete(completion: @escaping (Error?) -> Void)
 }
 
-final class LocalRestaurantLoader {
-	let cache: CacheClient
-	let currentDate: () -> Date
+public final class LocalRestaurantLoader {
+	private let cache: CacheClient
+	private let currentDate: () -> Date
 
-	init(cache: CacheClient, currentDate: @escaping () -> Date) {
+	public init(cache: CacheClient, currentDate: @escaping () -> Date) {
 		self.cache = cache
 		self.currentDate = currentDate
 	}
 
-	func save(_ items: [RestaurantItem], completion: @escaping (Error?) -> Void) {
+	public func save(_ items: [RestaurantItem], completion: @escaping (Error?) -> Void) {
 		cache.delete { [weak self] error in
 			guard let self else { return }
 			guard let error else {
